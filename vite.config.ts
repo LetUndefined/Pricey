46a15318ec124d05c2b7971869d7812959c33c51
Sandro <sandroodangelo@hotmail.com>
2026-04-21 17:02:20 +0200

    fix base url for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,10 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/Costly/' : '/',
+export default defineConfig({
+  base: '/',
   plugins: [react(), tailwindcss()],
   server: {
     host: true,
   },
-}))
+})
